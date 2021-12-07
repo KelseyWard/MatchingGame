07e94fd861d96d48c8238f74b601dd00a21c0ab2
Sogah2706 <94487123+Sogah2706@users.noreply.github.com>
2021-12-06 22:39:25 -0500

    Add files via upload

---
+++ Game/SplashScreen.java
@@ -0,0 +1,25 @@
+package com.zybooks.matchinggame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
